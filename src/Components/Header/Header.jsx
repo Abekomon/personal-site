3680b7e37d93d6ef8aed38a8264a8540928be7a7
Max Lange <max@langes.us>
2023-05-15 17:23:25 -0400

    Header component basic setup

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import "./Header.css"
+
+export default function Header() {
+  return (
+    <header className="main-header">
+      <h1>Max Lange</h1>
+      <p>Socials</p>
+    </header>
+  )
+}
