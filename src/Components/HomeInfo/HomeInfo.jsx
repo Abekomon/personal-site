45daf438094ffc3491c3dcdfae69a9fbc0353798
Max Lange <max@langes.us>
2023-05-16 12:22:53 -0400

    Add scaffolding for the home view components

---
+++ src/Components/HomeInfo/HomeInfo.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function HomeInfo() {
+  return(
+    <div className="home-info-box">
+      <aside>
+        <p>This is the picture</p>
+      </aside>
+      <article className="home-info">
+        <p>This is the information box</p>
+      </article>
+    </div>
+  )
+}
---
+++ src/Components/HomeNav/HomeNav.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+export default function HomeNav() {
+  return (
+    <nav className="home-nav">
+      <p>This is the home nav</p>
+    </nav>
+  )
+}
