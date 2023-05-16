c43631243eedbd1d64ca9528c189cef4c015152c
Max Lange <max@langes.us>
2023-05-16 12:41:06 -0400

    Import + add className for Home info, then add basic styling

--- src/Components/HomeInfo/HomeInfo.jsx
+++ src/Components/HomeInfo/HomeInfo.jsx
@@ -1,9 +1,10 @@
 import React from "react"
+import "./HomeInfo.css"
 
 export default function HomeInfo() {
   return(
     <div className="home-info-box">
-      <aside>
+      <aside className="home-img">
         <p>This is the picture</p>
       </aside>
       <article className="home-info">
