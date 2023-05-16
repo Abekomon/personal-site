d27add908bf89ba160c30dc68dd80cb1692fe345
Max Lange <max@langes.us>
2023-05-16 12:41:38 -0400

    Update HomeNav to include Links to other pages

--- src/Components/HomeNav/HomeNav.jsx
+++ src/Components/HomeNav/HomeNav.jsx
@@ -1,10 +1,16 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import "./HomeNav.css"
 
 export default function HomeNav() {
   return (
     <nav className="home-nav">
-      <p>This is the home nav</p>
+      <Link to="/projects">Projects</Link>
+      <Link to="/about-me">About Me</Link>
+      <div className="currently-learning">
+        <h3>Currently Learning:</h3>
+        <p>.NET frameworks!</p>
+      </div>
     </nav>
   )
 }
