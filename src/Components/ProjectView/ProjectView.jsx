d63e757adae7c96dd0b32b5515baa941ad3a9c71
Max Lange <max@langes.us>
2023-05-17 20:10:02 -0400

    Add project view boxing and component imports

--- src/Components/ProjectView/ProjectView.jsx
+++ src/Components/ProjectView/ProjectView.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React from "react"
+import ProjectGrid from "../ProjectGrid/ProjectGrid"
+import "./ProjectView.css"
+import { Link } from "react-router-dom"
 
 export default function ProjectView() {
   return (
-    <h2>This is the Project View</h2>
+    <div className="projects-view-container">
+      <Link className="back-home-link" to="/">{"< Back to Home"}</Link>
+      <div className="projects-box">
+        <div className="page-header">
+          <h2>Projects</h2>
+          <div>Box for Filtering</div>
+        </div>
+        <ProjectGrid />
+      </div>
+    </div>
   )
 }
