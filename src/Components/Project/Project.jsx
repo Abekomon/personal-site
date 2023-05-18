84973d0385ed69471d247d21231cf2f694ca1722
Max Lange <max@langes.us>
2023-05-17 20:22:54 -0400

    Removed unnecessary 'Created' text

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -5,8 +5,11 @@ export default function Project({info}) {
   const {name, date} = info
   return (
     <div className="project-box">
-      <h3>{name}</h3>
-      <p>{`Created ${date}`}</p>
+      <div>Screenshot here</div>
+      <div className="info-box">
+        <h3>{name}</h3>
+        <p>{date}</p>
+      </div>
     </div>
   )
 }
