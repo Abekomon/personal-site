542b52113409f5b8ca9c3eb79955cfa74b15a326
Max Lange <max@langes.us>
2023-05-20 19:52:07 -0400

    Add onClick to projects to open Modal

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React from "react"
 import "./Project.css"
 
-export default function Project({info}) {
+export default function Project({info, updateModal}) {
   const {name, date} = info
+
   return (
-    <div className="project-box">
+    <div className="project-box" onClick={() => updateModal(info, true)}>
       <div>Screenshot here</div>
       <div className="info-box">
         <h3>{name}</h3>
