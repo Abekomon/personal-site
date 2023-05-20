4c6a33072dc400b7505ca4e1936fa1e53b9aedaa
Max Lange <max@langes.us>
2023-05-20 19:53:23 -0400

    Map over imported projects, as well as add modal. Create updateModal function to handle opening/closing

--- src/Components/ProjectGrid/ProjectGrid.jsx
+++ src/Components/ProjectGrid/ProjectGrid.jsx
@@ -1,14 +1,30 @@
-import React from "react"
+import React, { useState } from "react"
 import "./ProjectGrid.css"
 import Project from "../Project/Project"
+import ProjectModal from "../ProjectModal/ProjectModal"
 import { projects } from "../../assets/projectList"
 
 export default function ProjectGrid() {
-  const projectList = projects.map(proj => <Project info={proj} />)
-
+  const [isOpen, setIsOpen] = useState(false)
+  const [curProj, setCurProj] = useState({})
+  
+  const updateModal = (data, modalState) => {
+    setCurProj(data)
+    setIsOpen(modalState)
+  }
+  
+  const projectList = projects.map((proj, i) => <Project key={i} info={proj} updateModal={updateModal} />)
+  
   return (
-    <div className="projects-grid">
-      {projectList}
-    </div>
+    <>
+      <ProjectModal
+        isOpen={isOpen}
+        data={curProj}
+        updateModal={updateModal}
+      />
+      <div className="projects-grid">
+        {projectList}
+      </div>
+    </>
   )
 }
