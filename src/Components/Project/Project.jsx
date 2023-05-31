5d87f912005d076c6b470f7870273a97856ae5a9
Max Lange <max@langes.us>
2023-05-31 13:33:18 -0400

    Update onclick functionality, as well as add changing background colors

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -1,12 +1,12 @@
 import React from "react"
 import "./Project.css"
 
-export default function Project({info, updateModal}) {
+export default function Project({info, updateProject, setStyle}) {
   const {name, date} = info
+  const handler = () => { updateProject(info, name) }
 
   return (
-    <div className="project-box" onClick={() => updateModal(info, true)}>
-      <div>Screenshot here</div>
+    <div className={`project-box ${setStyle(name)}`} onClick={handler}>
       <div className="info-box">
         <h3>{name}</h3>
         <p>{date}</p>
