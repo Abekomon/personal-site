e833193a8333eb4f07c2919b2a9b7b4b02ea245e
Max Lange <max@langes.us>
2023-05-31 13:25:24 -0400

    Update App.jsx to only have one route,  and remove extra imports

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -2,19 +2,14 @@ import { useState } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Header from '../Header/Header'
 import HomeView from '../HomeView/HomeView'
-import ProjectView from '../ProjectView/ProjectView'
-import AboutView from '../AboutView/AboutView'
 import Error from '../Error/Error'
 import './App.css'
 
 function App() {
   return (
     <main>
-      <Header />
       <Switch>
         <Route exact path="/" component={HomeView}/>
-        <Route exact path="/about-me" component={AboutView}/>
-        <Route exact path="/projects" component={ProjectView}/>
         <Route component={Error}/>
       </Switch>
     </main>
