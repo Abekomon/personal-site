80dd4e3a9966f0bd03650f3e9ca3a017c94b8cd8
Max Lange <max@langes.us>
2023-05-31 13:28:26 -0400

    Move larger text block from home view to about, and style boxes

--- src/Components/AboutView/AboutView.jsx
+++ src/Components/AboutView/AboutView.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import "./AboutView.css";
 
 export default function AboutView() {
   return (
-    <h2>This is the About View</h2>
+    <section id="about" className="about-view">
+      <h2>About Me</h2>
+      <div className="about-info-section">
+        <div className="about-img">
+          Some random picture
+        </div>
+        <div className="about-text">
+          <p>
+            A Front End Software Engineer that brings a unique perspective 
+            to the development process by leveraging past experience in 
+            customer service. Skilled in empathizing with users and understanding 
+            their needs, and translating that understanding into building intuitive 
+            and user-friendly interfaces, with a passion for creating visually appealing, 
+            responsive, and pixel-perfect designs that enhance the user's journey.
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
