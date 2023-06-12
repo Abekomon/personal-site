b9e5cfc5476c763cd26d580455b24c8b9275d0dc
Max Lange <max@langes.us>
2023-06-12 13:06:11 -0400

    Update swiper to use the most recent syntax

--- src/Components/ProjectDisplay/ProjectDisplay.jsx
+++ src/Components/ProjectDisplay/ProjectDisplay.jsx
@@ -1,9 +1,11 @@
-import React, {useEffect} from "react";
+import React from "react";
 import "./ProjectDisplay.css"
 import githubIcon from "../../assets/Icons/github.svg"
 import linkIcon from "../../assets/Icons/link.svg"
-import {register} from 'swiper/element/bundle'
-register()
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
 
 export default function ProjectDisplay({info}) {
   const {name, date, repo_link, deployed_link, screenshots, desc, bullets, tech} = info
@@ -12,55 +14,40 @@ export default function ProjectDisplay({info}) {
     specList = bullets.map((bul, i) => <li key={i}>{bul}</li>)
     imgList = screenshots.map((image, i) => {
       return (
-        <swiper-slide key={i}>
+        <SwiperSlide key={i}>
           <img className="swiper-image" src={image} />
-        </swiper-slide>
+        </SwiperSlide>
       )
     })
   }
-
-  // useEffect(() => {
-  //   const swiperEl = document.querySelector('swiper-container');
-  //   const swiperParams = {
-  //     breakpoints: {
-  //       1000: {
-  //         slidesPerView: 3,
-  //       },
-  //       700: {
-  //         slidesPerView: 2,
-  //       }
-  //     }
-  //   };
-  //   Object.assign(swiperEl, swiperParams);
-  //   swiperEl.initialize();
-  // }, [])
 
   return (
     <div className="project-display">
       {!name ? <p className="proj-placeholder">Select a project!</p> : 
       <>
-        <swiper-container
-          grab-cursor="true"
-          navigation="true"
-          loop="true"
+        <Swiper
+          navigation={true} 
+          modules={[Navigation]}
         >
           {imgList}
-        </swiper-container>
-        <h3>{name}</h3>
-        <p>{date}</p>
-        <p>{desc}</p>
-        <ul className="spec-list">
-          {specList}
-        </ul>
-        <div className="proj-linkbox">
-          <a className="proj-link" target="_blank" href={repo_link}>
-            <img className="proj-link-icon" src={githubIcon} />
-            Github Repo
-          </a>
-          <a className="proj-link" target="_blank" href={deployed_link}>
-            <img className="proj-link-icon" src={linkIcon} /> 
-            Live Demo
-          </a>
+        </Swiper>
+        <div className="proj-info-box">
+          <h3>{name}</h3>
+          <p>{date}</p>
+          <p>{desc}</p>
+          <ul className="spec-list">
+            {specList}
+          </ul>
+          <div className="proj-linkbox">
+            <a className="proj-link" target="_blank" href={repo_link}>
+              <img className="proj-link-icon" src={githubIcon} />
+              Github Repo
+            </a>
+            <a className="proj-link" target="_blank" href={deployed_link}>
+              <img className="proj-link-icon" src={linkIcon} /> 
+              Live Demo
+            </a>
+          </div>
         </div>
       </>
       }
