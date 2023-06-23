fa818c04e6ca0c2949c25bbcf9158725d4ed97db
Max Lange <max@langes.us>
2023-06-23 18:47:02 -0400

    Start contact view

--- src/Components/ContactView/ContactView.jsx
+++ src/Components/ContactView/ContactView.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import "./ContactView.css"
+import "./ContactView.css";
+import emailIcon from "../../assets/Icons/email.svg";
 
 export default function ContactView() {
   return (
     <section id="contact" className="contact-view">
-
+      <h2>Contact</h2>
+      <div className="email-box">
+        <img className="email-icon" src={emailIcon} /> 
+        <h3>Mail:</h3>
+        <p>max@langes.us</p>
+      </div>
     </section>
   )
 }
